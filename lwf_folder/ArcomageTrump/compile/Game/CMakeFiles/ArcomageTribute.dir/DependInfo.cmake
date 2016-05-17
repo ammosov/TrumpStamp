6bb4a70e075c0a5bc58f33c74920217d02037ac4
lockywolf <lockywolf@gmail.com>
2016-05-17 19:08:27 +0100

    Added arcomage code.

---
+++ lwf_folder/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/DependInfo.cmake
@@ -0,0 +1,99 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Games/ArcomageTrump/Game/ThirdParty/minizip/ioapi.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/minizip/ioapi.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/minizip/unzip.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/minizip/unzip.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/adler32.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/adler32.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/compress.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/compress.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/crc32.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/crc32.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/deflate.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/deflate.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/gzclose.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/gzclose.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/gzlib.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/gzlib.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/gzread.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/gzread.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/gzwrite.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/gzwrite.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/infback.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/infback.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/inffast.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/inffast.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/inflate.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/inflate.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/inftrees.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/inftrees.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/trees.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/trees.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/uncompr.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/uncompr.c.obj"
+  "C:/Games/ArcomageTrump/Game/ThirdParty/zlib/src/zutil.c" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/ThirdParty/zlib/src/zutil.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../boost_1_60_0"
+  "../SDL-1.2.15/include"
+  "../SDL_mixer-1.2.12/include"
+  "../SDL_ttf-2.0.11/include"
+  "../SDL_image-1.2.12/include"
+  "../LUA5.1/include"
+  "../Game/Include"
+  "../Game/Include/txml"
+  "../Game/Include/Lua"
+  "../Game/Include/System"
+  "../Game/Include/UI"
+  "../Game/ThirdParty/minizip"
+  "../Game/ThirdParty/zlib/include"
+  "../Game/ThirdParty/zlib/win32"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Games/ArcomageTrump/Game/Include/txml/tinystr.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Include/txml/tinystr.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Include/txml/tinyxml.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Include/txml/tinyxml.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Include/txml/tinyxmlerror.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Include/txml/tinyxmlerror.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Include/txml/tinyxmlparser.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Include/txml/tinyxmlparser.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/System/AeonCompress.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/System/AeonCompress.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/System/ZIPFile.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/System/ZIPFile.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/anim_base.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/anim_base.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/anim_card.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/anim_card.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/card.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/card.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/config.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/config.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/cplayer.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/cplayer.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/deck.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/deck.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/deckpreview.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/deckpreview.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/enemy.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/enemy.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/engine.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/engine.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/game.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/game.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/main.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/main.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/player.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/player.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/script.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/script.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/scriptbind.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/scriptbind.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/ui_button.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/ui_button.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/ui_card.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/ui_card.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/ui_image.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/ui_image.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/ui_manager.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/ui_manager.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/ui_object.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/ui_object.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/ui_text.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/ui_text.cpp.obj"
+  "C:/Games/ArcomageTrump/Game/Source/utility.cpp" "C:/Games/ArcomageTrump/compile/Game/CMakeFiles/ArcomageTribute.dir/Source/utility.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../boost_1_60_0"
+  "../SDL-1.2.15/include"
+  "../SDL_mixer-1.2.12/include"
+  "../SDL_ttf-2.0.11/include"
+  "../SDL_image-1.2.12/include"
+  "../LUA5.1/include"
+  "../Game/Include"
+  "../Game/Include/txml"
+  "../Game/Include/Lua"
+  "../Game/Include/System"
+  "../Game/Include/UI"
+  "../Game/ThirdParty/minizip"
+  "../Game/ThirdParty/zlib/include"
+  "../Game/ThirdParty/zlib/win32"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

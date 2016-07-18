f837a98b9c2824664b84be8f801f345c49c4d45c
lockywolf <lockywolf@gmail.com>
2016-07-18 11:58:50 +0100

    Added some screen transitions.

--- Trump-Kivy/main.py
+++ Trump-Kivy/main.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 class LoginScreen(GridLayout):
@@ -17,11 +19,59 @@ class LoginScreen(GridLayout):
         self.add_widget(self.password)
 
 
-class MyApp(App):
+# class MyApp(App):
+#
+#     def build(self):
+#         return LoginScreen()
+#
+#
+# if __name__ == '__main__':
+#     MyApp().run()
+
+Builder.load_string("""
+<MenuScreen>:
+    GridLayout:
+        cols:  1
+        Button:
+            text: 'Play'
+        Button:
+            text: 'Goto settings'
+            on_press: root.manager.current = 'settings'
+        Button:
+            text: 'About...'
+        Button:
+            text: 'Quit'
+
+
+
+<SettingsScreen>:
+    BoxLayout:
+        Button:
+            text: 'My settings button'
+        Button:
+            text: 'Back to menu'
+            on_press: root.manager.current = 'menu'
+""")
+
+
+# Declare both screens
+class MenuScreen(Screen):
+    pass
+
+
+class SettingsScreen(Screen):
+    pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
+
+
+class TestApp(App):
 
     def build(self):
-        return LoginScreen()
-
+        return sm
 
 if __name__ == '__main__':
-    MyApp().run()
+    TestApp().run()

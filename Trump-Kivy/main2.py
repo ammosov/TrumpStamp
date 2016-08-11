e5468905ee0f758e0b49f64456e540eb2f7c28d4
lockywolf <lockywolf@gmail.com>
2016-08-11 11:51:19 +0100

    Some class structure added. Sounds stubs added.

--- Trump-Kivy/main2.py
+++ Trump-Kivy/main2.py
@@ -5,8 +5,8 @@ kivy.require('1.9.1')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty,\
-    ObjectProperty
+from kivy.properties import BoundedNumericProperty, ReferenceListProperty,\
+    ObjectProperty, ListProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 
@@ -16,17 +16,43 @@ from kivy.core.audio import SoundLoader
 
 
 class TrumpCard( Button ):
-    myWavSound = SoundLoader.load('assets/stubs/Sounds/harp.wav')
+    """" This is a GUI class, representing a game field object."""
+    myWavSound = SoundLoader.load('assets/stubs/Sounds/card.wav')
 
     def play_card(self):
 
-        self.parent.scoreTrump += 1
+        # self.parent.scoreTrump += 1
         self.myWavSound.play()
 
 
+class TrumpPlayer(Widget):
+    partisans = BoundedNumericProperty(0, min=0, max=125, rebind=True)
+    swing_voters = BoundedNumericProperty(0, min=0)
+
+    media = BoundedNumericProperty(1, min=1, max=100)
+    news = BoundedNumericProperty(1, min=0, max=300)
+    mojo = BoundedNumericProperty(1, min=1, max=100)
+    charisma = BoundedNumericProperty(1, min=0, max=300)
+    donors = BoundedNumericProperty(1, min=1, max=100)
+    cash = BoundedNumericProperty(1, min=0, max=300)
+
+    cards_actions = ListProperty([])  # Should have a list of card actions
+
+
+class TrumpPlayerTrump(TrumpPlayer):
+    pass
+
+
+class TrumpPlayerHillary(TrumpPlayer):
+    pass
+
+
 class TrumpGame(FloatLayout):
-    scoreTrump = NumericProperty(0)
-    scoreHillary = NumericProperty(0)
+    """This class represents the game. As a Kivy object it represents the game field and is a root for all other
+    objects. As general class it stores all the stuff in the game.
+    """
+    myPlayerTrump = TrumpPlayerTrump()
+    myPlayerHillary = TrumpPlayerHillary()
 
 
 class TrumpApp(App):

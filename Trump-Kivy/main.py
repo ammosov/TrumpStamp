7582b7516721bdee6b747c0d13c685bc29d1e23c
greenriverrus <vadim-mazaev@mail.ru>
2016-09-24 13:19:53 +0300

    Some commit

--- Trump-Kivy/main.py
+++ Trump-Kivy/main.py
@@ -1,13 +1,7 @@
-import kivy
 from kivy.app import App
 from kivy.config import Config
-from kivy.core.audio import SoundLoader
 from kivy.logger import Logger
-from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-
-from kivy.properties import BoundedNumericProperty, ListProperty, ObjectProperty
 from gameclasses_v3_0 import *
 
 kivy.require('1.7.2')
@@ -21,13 +15,14 @@ class ElectionsGame(FloatLayout):
     """This class represents the game. As a Kivy object it represents the game field and is a root for all other
     objects. As a general class it stores all the stuff in the game.
     """
-    deck = ObjectProperty(None)
-    playerTrump = ObjectProperty(PlayerKv())
-    playerHillary = ObjectProperty(PlayerKv())
+    # deck = ObjectProperty(None)
+    # playerTrump = ObjectProperty(Player)
+    # playerHillary = ObjectProperty(Player)
+
 
     def __init__(self, **kwargs):
         super(FloatLayout, self).__init__(**kwargs)
-        #self.add_widget(PlayerKv())
+        self.game_master = GameMaster(0)
 
 class ElectionsApp(App):
     def build(self):
